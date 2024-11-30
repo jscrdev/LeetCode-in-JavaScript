1db3c541558d83270b5a52cc40a72ec01d0d25f3
Valentyn Kolesnikov <javadev75@gmail.com>
2024-11-30 18:14:34 +0200

    Fixed test 1

--- src/test/js/g0001_0100/s0001_two_sum/solution.test.js
+++ src/test/js/g0001_0100/s0001_two_sum/solution.test.js
@@ -1,23 +1,19 @@
 // tslint:disable:no-magic-numbers
-import { ListNode } from 'src/main/js/com_github_leetcode/listnode'
-import { addTwoNumbers } from 'src/main/js/g0001_0100/s0002_add_two_numbers/solution'
+import { twoSum } from 'src/main/js/g0001_0100/s0001_two_sum/solution'
 import { expect, test } from 'vitest'
 
-test('addTwoNumbers', () => {
-    expect(
-        addTwoNumbers(
-            new ListNode(2, new ListNode(4, new ListNode(3))),
-            new ListNode(5, new ListNode(6, new ListNode(4))),
-        ).toString(),
-    ).toEqual(new ListNode(7, new ListNode(0, new ListNode(8))).toString())
+test('twoSum', () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
 })
 
-test('addTwoNumbers2', () => {
-    expect(new ListNode(7).val).toEqual(7)
+test('twoSum2', () => {
+    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
 })
 
-test('addTwoNumbers3', () => {
-    expect(addTwoNumbers(new ListNode(5), new ListNode(5)).toString()).toEqual(
-        new ListNode(0, new ListNode(1)).toString(),
-    )
+test('twoSum3', () => {
+    expect(twoSum([3, 3], 6)).toEqual([0, 1])
+})
+
+test('twoSum4', () => {
+    expect(twoSum([3, 3], 7)).toEqual([-1, -1])
 })
