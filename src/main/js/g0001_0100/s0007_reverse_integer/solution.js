44a9884d6b42113ec83cce6ef31f1a005e1ba59c
Valentyn Kolesnikov <javadev75@gmail.com>
2024-12-04 17:40:54 +0200

    Improved task 7

--- src/main/js/g0001_0100/s0007_reverse_integer/solution.js
+++ src/main/js/g0001_0100/s0007_reverse_integer/solution.js
@@ -1,24 +1,20 @@
 // #Medium #Top_Interview_Questions #Math #Udemy_Integers
-// #2024_12_03_Time_66_ms_(87.91%)_Space_52_MB_(94.57%)
+// #2024_12_04_Time_67_ms_(85.38%)_Space_53.1_MB_(34.57%)
 
 /**
  * @param {number} x
  * @return {number}
  */
 var reverse = function (x) {
-    let r = x
-    if (r < 0) {
-        r = r.toString().split('')
-        let a = r.shift()
-        r.reverse()
-        r.unshift('-')
-
-        if (Number(r.join('')) > -2147483647) {
-            return Number(r.join(''))
-        }
+    let rev = 0
+    while (x !== 0) {
+        rev = rev * 10 + (x % 10)
+        x = Math.trunc(x / 10)
     }
-    let reverseN = Number(r.toString().split('').reverse().join(''))
-    return reverseN < 2147483647 ? reverseN : 0
+    if (rev > Math.pow(2, 31) - 1 || rev < -Math.pow(2, 31)) {
+        return 0
+    }
+    return rev
 }
 
 export { reverse }
